2c4c65688123e2200a69bb534d21d5b957c2a9b2
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2024-08-23 08:21:30 -0300

    Lavacar senges

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// JavaScript básico para futuras funcionalidades, como envio de formulário
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Página carregada!');
-    // Código para envio de formulário, animações, etc.
-});
